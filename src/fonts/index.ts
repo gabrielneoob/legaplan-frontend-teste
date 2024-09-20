ee163a1d1353033512337a9642f3d82caf5fa873
Gabriel Neoob <gabrielngc@gmail.com>
2024-09-20 16:17:06 -0300

    feat(fonts): add Inter_Tight font

---
+++ src/fonts/index.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line camelcase
+import { Inter_Tight } from 'next/font/google'
+
+export const interTight = Inter_Tight({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+})
