c5f657cf134f196e641fc6eaad5f5cbb9de58871
Gabriel Neoob <gabrielngc@gmail.com>
2024-09-20 16:07:30 -0300

    feat(types): Add all features types for the application

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,4 +7,17 @@ export interface Button {
 export interface TaskProps {
   title: string
   checked: boolean
+  id: string
+}
+
+export interface TaskContextProps {
+  tasks: TaskProps[]
+  setTasks: React.Dispatch<React.SetStateAction<TaskProps[]>>
+}
+
+export interface ModalProps {
+  type: 'delete' | 'add'
+  title: string
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>
+  id?: string
 }
